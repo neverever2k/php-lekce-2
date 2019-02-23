c163471d907c413d22fdc3e4021b26a664bd5f1a
neverever2k <pavelurban2@centrum.cz>
2019-02-23 15:00:27 +0100

    pythagoras

---
+++ Pythagoras.php
@@ -0,0 +1,27 @@
+<?php
+$a = '10';
+$b = '20';
+$obdelnikObsah = $a * $b;?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>Pythagoras matematika</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="starter-template.css" rel="stylesheet">
+</head>
+
+<body>
+
+Obsah obdelniku o stranach: <?php echo "a: $a cm b: $b cm je $obdelnikObsah cm2";?>
+</body>
